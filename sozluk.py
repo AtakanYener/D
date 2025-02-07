80cbf13da29f791e44246732c5ab2de63a1042a2
AtakanYener <atakanyene@gmail.com>
2025-02-07 15:48:22 +0300

    Update sozluk.py

--- sozluk.py
+++ sozluk.py
@@ -1,11 +1,19 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
+            "GOAT" : "Greatest of all time yani tüm zamanların en iyisi",
+            "Based" : "Bir düşüncenin doğru olduğunu belirtmek",
+            "W" : "Bir şeyi iyi olarak değerlendirmek",
+            "L" : "Bir şeyi kötü olarak değerlendirmek",
+            "NPC" : "Sıkıcı veya özgün olmayan insanlar için kullanılır."
             }
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
-if word in meme_dict.keys():
-    # Kelime eşleşiyorsa ne yapmalıyız?
-    print(meme_dict[word] , "anlamına gelmektedir.")
-else:
-    # Kelime eşleşmiyorsa ne yapmalıyız?
-    print("böyle bir kelime yok.")
+while True:
+            for i in range(len(meme_dict)):
+                        print(meme_dict[i])
+            word = input("Anlamadığınız bir kelime yazın (yukarıdaki gibi yazın!): ")
+            if word in meme_dict.keys():
+                # Kelime eşleşiyorsa ne yapmalıyız?
+                print(meme_dict[word] , "anlamına gelmektedir.")
+            else:
+                # Kelime eşleşmiyorsa ne yapmalıyız?
+                print("böyle bir kelime yok.")
